c269cdb54aa852f1ec92eacdc137ce1778d78f30
Dharshinideva123 <90498022+Dharshinideva123@users.noreply.github.com>
2021-11-19 14:38:33 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,14 +1,19 @@
-package Forloop_Ex8;
-//Program to print a text 5 times
+package ExceptionHandling_Ex6;
 
 class Main {
   public static void main(String[] args) {
-	int n = 5;
-	// for loop  
-	for (int i = 1; i <= n; ++i) {
-	  System.out.println("Java is fun");
+	try {
+      // code that generates exception
+	  int divideByZero = 5 / 0;
+	}
+	
+	catch (ArithmeticException e) {
+	  System.out.println("ArithmeticException => "+ e.getMessage());
+	}
+	
+	finally {
+	  System.out.println("This is the finally block");
 	}
   }
-}	
 
-	
+}
